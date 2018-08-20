553950cde8d4cde056b3357c0e275a5dc1f8b1d6
tamata78 <deveenjhalf7@gmail.com>
2018-08-20 22:12:13 +0900

    init commit

---
+++ seleniumUtil.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+class SeleniumUtil:
+
+    @staticmethod
+    def switch_other_tab(driver, window_handle_len):
+        current_handle_len = len(driver.window_handles)
+        WebDriverWait(driver, 3).until(lambda driver: current_handle_len > window_handle_len)
+        driver.switch_to.window(driver.window_handles[current_handle_len - 1])
+
+        return current_handle_len
