04a2501dbb5c31d3e9483ae87e79349fabaf5f34
tamata78 <deveenjhalf7@gmail.com>
2018-08-22 21:19:38 +0900

    improve exec speed

--- usa_settlementNotice.py
+++ usa_settlementNotice.py
@@ -84,6 +84,9 @@ class UsaSettlementNotice():
 
             stockInfo["stockCd"] = elTds[0].find_element_by_tag_name('div').text.split()[0]
             stockInfo["stockNm"] = elTds[0].find_element_by_tag_name('a').text
+            stockInfo["profitAnnoDay"] = '-'
+            stock_url = "https://www.morningstar.co.jp/frstock_us/stock.html?symbol1=" + stockInfo["stockCd"]
+            stockInfo["stock_info"] = "<" + stock_url + "|stock_info>"
 
             stockInfoList.append(stockInfo)
 
@@ -94,24 +97,31 @@ class UsaSettlementNotice():
         finance_url = "https://www.sbisec.co.jp/ETGate/?_ControlID=WPLETmgR001Control&_PageID=WPLETmgR001Mdtl20&_DataStoreID=DSWPLETmgR001Control&_ActionID=DefaultAID&burl=iris_economicCalendar&cat1=market&cat2=economicCalender&dir=tl1-cal%7Ctl2-schedule%7Ctl3-foreign%7Ctl4-US&file=index.html&getFlg=on"
         driver.get(finance_url)
 
+        # setting sortedsettleInfo
+        settleList = driver.find_elements_by_xpath('//*[@id="MAINAREA01"]/table/tbody/tr')
+        settleInfoList = []
+        for settle in settleList:
+            settleTds = settle.find_elements_by_tag_name('td')
+
+            settleInfo = {}
+            settleInfo["stockCd"] = settleTds[0].text
+            settleInfo["profitAnnoDay"] = settleTds[2].text
+            settleInfoList.append(settleInfo)
+
+        sortedSettleInfoList = sorted(settleInfoList, key=itemgetter('stockCd'))
+
+        profitAnnoDay = ''
         for stockInfo in stockInfoList:
             stockCd = stockInfo["stockCd"]
 
-            # set profitAnnoDay
-            settleList = driver.find_elements_by_xpath('//*[@id="MAINAREA01"]/table/tbody/tr')
-            profitAnnoDay = ''
-            for settle in settleList:
-                settleTds = settle.find_elements_by_tag_name('td')
-                if settleTds[0].text == stockInfo["stockCd"]:
-                    profitAnnoDay = settleTds[2].text
+            for sortedSettleInfo in sortedSettleInfoList:
+                if sortedSettleInfo["stockCd"] == stockInfo["stockCd"]:
+                    stockInfo["profitAnnoDay"] = sortedrSettleInfo["profitAnnoDay"]
+                    break
 
-            if profitAnnoDay:
-                stockInfo["profitAnnoDay"] = profitAnnoDay
-            else:
-                stockInfo["profitAnnoDay"] = '-'
-
-            stock_url = "https://www.morningstar.co.jp/frstock_us/stock.html?symbol1=" + stockCd
-            stockInfo["stock_info"] = "<" + stock_url + "|stock_info>"
+                if sortedSettleInfo["stockCd"] > stockInfo["stockCd"]:
+                    # no exist after a target stockCd
+                    break
 
 if __name__ == "__main__":
     settle = UsaSettlementNotice()
