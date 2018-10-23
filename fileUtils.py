13b4b965445a4279bba4f393ee697ca6d0b7caa2
tamata78 <deveenjhalf7@gmail.com>
2018-10-23 21:44:07 +0900

    close file by with as

--- fileUtils.py
+++ fileUtils.py
@@ -2,13 +2,13 @@
 import json
 import os
 
+
 class FileUtils:
+
     @staticmethod
     def open_file(exec_file, openfileName):
         exec_file_path = os.path.dirname(os.path.abspath(exec_file))
-        f = open(exec_file_path + openfileName, 'r')
-        config = json.load(f)
-        f.close()
+        with open(exec_file_path + openfileName, 'r') as f:
+            config = json.load(f)
 
         return config
-
